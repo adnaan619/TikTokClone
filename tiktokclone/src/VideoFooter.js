032e229aa6656b1187417b03c93cfb4ac9d39777
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-06-23 03:20:00 -0700

    material-ui icons imported

---
+++ tiktokclone/src/VideoFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './VideoFooter.css';
+import MusicNoteIcon from '@material-ui/icons/MusicNoteIcon';
+
+
+function VideoFooter() {
+  return (
+    <div className='videoFooter'>
+        <div className='videoFooter__text'></div>
+        <h3>@adnaanfuard</h3>
+        <p>this is a description</p>
+        <MusicNoteIcon/>
+        <img class
