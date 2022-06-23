9c063de8426436d8f2fcbb0e24f54ef56616767e
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-06-23 00:24:08 -0700

    the layout created for the application giving it both the width and the height and figure size

--- tiktokclone/src/App.js
+++ tiktokclone/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Video from './Video';
+import "./App.css";
 
 function App() {
   return (
     //BEM 
     <div className="app">
       <h1>Hello Clever Programmers, Lets build the next web</h1>
+
+      <div className="app__videos"> 
+        <Video/>
+        <Video/>
+        <Video/>
+        <Video/>
+      </div>
     </div>
   );
 }
