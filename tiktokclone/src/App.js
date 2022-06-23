112ad35bbcad6abd026cd2b03dc4a5fded486888
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-06-23 01:35:53 -0700

    video link from tiktok taken and fixing the rendering and fitting the video to scale

--- tiktokclone/src/App.js
+++ tiktokclone/src/App.js
@@ -6,14 +6,15 @@ function App() {
   return (
     //BEM 
     <div className="app">
-      <h1>Hello Clever Programmers, Lets build the next web</h1>
-
-      <div className="app__videos"> 
-        <Video/>
-        <Video/>
-        <Video/>
-        <Video/>
-      </div>
+      <center>
+        <div className="app__videos"> 
+          <Video/>
+          <Video/>
+          <Video/>
+          <Video/>
+        </div>
+      </center>
+      
     </div>
   );
 }
--- tiktokclone/src/Video.js
+++ tiktokclone/src/Video.js
@@ -5,7 +5,11 @@ import './Video.css';
 function Video() {
   return (
     <div className="video">
-        <h1>This is a video</h1>
+        <video 
+          className="video__player" 
+          loop
+          controls
+          src="https://v16-webapp.tiktok.com/12b018e0a09b48c5af956dc45d5be93f/62b3b9d3/video/tos/useast2a/tos-useast2a-pve-0037-aiso/04f124a3961e4d1e9106b4856f3c2d59/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2794&bt=1397&btag=80000&cs=0&ds=3&ft=eXd.6HNVMyq8ZG7IZwe2N9PQml7Gb&mime_type=video_mp4&qs=0&rc=PDRnZ2Q0OWQ3aTs0NjVpPEBpM3BnNTM6Zjw8ZDMzZjczM0A0YzNeYmMwX18xNDViMV9jYSNvNC82cjQwa2pgLS1kMWNzcw%3D%3D&l=202206221853580102452431360800F6A1"></video>
         {/* <VideoFooter /> */}
         {/* <VideoSidebar/> */}
     </div>
