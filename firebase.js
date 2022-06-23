d2cde162e02676a104af7d4e68d09a330ba7ed1d
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-06-22 23:58:25 -0700

    First commit - file structure planned out for the appication

---
+++ firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALG8w3MmJKj4giKFFkaf-1gEVJnRusskQ",
+  authDomain: "tik-tokclone-bda19.firebaseapp.com",
+  projectId: "tik-tokclone-bda19",
+  storageBucket: "tik-tokclone-bda19.appspot.com",
+  messagingSenderId: "427090152743",
+  appId: "1:427090152743:web:2ed37b40321b162544ad02",
+  measurementId: "G-VK02FDS289"
+};
---
+++ tiktokclone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    //BEM 
+    <div className="app">
+      <h1>Hello Clever Programmers, Lets build the next web</h1>
+    </div>
+  );
+}
+
+export default App;
