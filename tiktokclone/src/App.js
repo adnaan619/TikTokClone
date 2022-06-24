266fa07385d67f8f699114f756a2a402c90dcac5
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-06-23 22:58:13 -0700

    Video footer ticker added

--- tiktokclone/src/App.js
+++ tiktokclone/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Video from './Video';
-import "./App.css";
+
 
 function App() {
   return (
--- tiktokclone/src/VideoFooter.js
+++ tiktokclone/src/VideoFooter.js
@@ -6,13 +6,25 @@ import MusicNoteIcon from '@material-ui/icons/FavoriteBorder';
 function VideoFooter() {
   return (
     <div className='videoFooter'>
-        <div className='videoFooter__text'></div>
-        <h3>@adnaanfuard</h3>
-        <p>this is a description</p>
-        <MusicNoteIcon/>
-        <img className="videoFooter__record" src=""></img>
-    </div>
-  )
+        <div className='videoFooter__text'>
+          <h3>@adnaanfuard</h3>
+          <p>this is a description</p>
+          <div className='videoFooter__ticker'>
+            <MusicNoteIcon className="videoFooter__icon"/>
+            <Ticker mode="smooth">
+                {({ index }) => (
+                    <>
+                        <p>YOo whats up guys</p>
+                    </>
+                )}
+            </Ticker>
+          </div>  
+        </div>
+        <img 
+            className="videoFooter__record" 
+            src="https://static.thenounproject.com/png/"  alt=""></img>
+    </div>    
+  );
 }
 
 export default VideoFooter;
